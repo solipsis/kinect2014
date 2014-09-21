c55d61b9b51b924d63658f00b7a6fd0baace3aff
Dolphins2Go <jsantill@mymail.mines.edu>
2014-09-20 20:17:13 -0600

    *Crosses Fingers* Put in Launcher code

--- Kinect/Launcher.cs
+++ Kinect/Launcher.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
-namespace Kinect
-{
-    class Launcher
-    {
-    }
+namespace Kinect {
+
+	class Launcher {
+
+		private GameInfo gameInfo;
+
+		public Launcher(GameInfo info) {
+			// IDK, some stuff might go here.
+			gameInfo = info;
+		}
+
+		private void prepLaunch() {
+			ProcessStartInfo start = new ProcessStartInfo();
+
+			start.FileName = gameInfo.executablePath;
+
+			start.WindowStyle = ProcessWindowStyle.Normal;
+
+			launch(start);
+		}
+
+		private void launch(ProcessStartInfo start) {
+			using (Process proc = Process.Start(start)) {
+				proc.WaitForExit();
+
+				// Maybe check error codes here? I think grabbing proc.ExitCode might give us some useful info.
+
+				int exitCode = proc.ExitCode;
+			}
+		}
+
+	}
 }
