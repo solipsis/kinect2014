de6a1a9a6c771eede92241408ba802913a788f63
solipsis <dalexand@mines.edu>
2014-10-15 00:03:57 -0700

    gameButtons now contain executable path info

--- Kinect/GameButton.cs
+++ Kinect/GameButton.cs
@@ -10,11 +10,13 @@ namespace Kinect
     {
         public String Title { get; set; }
         public String Description { get; set; }
+        public String Path { get; set; }
 
-        public GameButton(string title, string description) : base()
+        public GameButton(string title, string description, string path) : base()
         {
             Title = title;
             Description = description;
+            Path = path;
         }
     }
 }
