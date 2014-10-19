426810f24706fc4bcfb416e66fa28ce5864589da
solipsis <davida3141@gmail.com>
2014-10-19 12:26:10 -0600

    added 1.8 kinect counter

---
+++ KinectCounter_old/KinectCounter_old/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+//using Microsoft.Speech;
+
+
+using Microsoft.Kinect;
+namespace KinectCounter_old
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show(KinectSensor.KinectSensors.Count.ToString() + " Kinect sensors detected" );
+        }
+    }
+}
