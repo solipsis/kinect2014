7ad82e5c88bdaedf457aa67b490b0ca9a92ce782
ccopper <ccopper@mymail.mines.edu>
2014-12-03 09:30:18 -0700

    Added more Comments to the ScoreAPI Code

--- ScoreAPI/ScoreAPI.cs
+++ ScoreAPI/ScoreAPI.cs
@@ -7,15 +7,14 @@ using System.Net;
 
 namespace Kinect.ScoreAPI
 {
-    public class DataObject
-    {
-        public string Name { get; set; }
-    }
 
     public class ScoreAPI
     {
+        //UPDATE THIS URL AS NECESSARY
         private const string URL = "http://luna.mines.edu/team07/ccopper/scoreAPI.php";
 
+        //This is just a simple test code for the API.
+        //To run change the project to a console app and run.
         static void Main(string[] args)
         {
             ScoreAPIResponse sr = SubmitScore("DummyGame", "Frank Smith", 500);
@@ -35,17 +34,24 @@ namespace Kinect.ScoreAPI
             Console.ReadLine();
         }
 
-
+        //Code is more or less identical for both API calls so just this one will be commented.
         public static ScoreAPIResponse RequestScores(string GameName, int Count, int StartPos)
         {
+            //This is the parameter string to pass the arguments to the endpoint
             string URLParam = String.Format("?methodName=requestScores&gameName={0}&count={1}&startPos={2}", GameName, Count, StartPos);
 
+            //Connections may fail at several points during the call.
+            //Catch any failure and return a valid ScoreAPIResponse
             try
             {
+                //Create Reques with a 1.5 second time out.  Default timeout is much to long and the calls do block
                 HttpWebRequest request = WebRequest.Create(URL + URLParam) as HttpWebRequest;
                 request.Timeout = 1500;
+                //Make the request and get the HTTP Respons from the server
                 using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
+                    //If the server returned anything other than HTTP 200(Ok)
+                    //Report the HTTP error code and a message in the ScoreAPI Response
                     if (response.StatusCode != HttpStatusCode.OK)
                     {
                         ScoreAPIResponse sr = new ScoreAPIResponse();
@@ -53,15 +59,18 @@ namespace Kinect.ScoreAPI
                         sr.ErrMsg = response.StatusDescription;
                         return sr;
                     }
+                    //Using the reponse convert th JSON returned to a ScoreAPIResponse object and return it
                     DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(ScoreAPIResponse));
                     object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
                     ScoreAPIResponse jsonResponse = objResponse as ScoreAPIResponse;
                     return jsonResponse;
                 }
+            //Connection failures, JSON converstion errors and anything else is caught here and returned with a status of -1
+            // Along with a message of the error
             } catch(Exception e)
             {
                 ScoreAPIResponse sr = new ScoreAPIResponse();
-                sr.ErrCode = 1;
+                sr.ErrCode = -1;
                 sr.ErrMsg = e.Message;
                 return sr;
             }
@@ -93,7 +102,7 @@ namespace Kinect.ScoreAPI
             catch (Exception e)
             {
                 ScoreAPIResponse sr = new ScoreAPIResponse();
-                sr.ErrCode = 1;
+                sr.ErrCode = -1;
                 sr.ErrMsg = e.Message;
                 return sr;
             }
